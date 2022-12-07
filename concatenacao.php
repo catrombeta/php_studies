327e7e6641b3301cf90e53169a28f3c0f08e89f8
Caroline Trombeta <carolinecampost@gmail.com>
2022-12-07 10:29:06 +0000

    07/12 - variaveis e concatenação

---
+++ concatenacao.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Concatenação</title>
+</head>
+<body>
+    <?php
+        $nome = 'Caroline Trombeta';
+        $idade = 26;
+
+        echo 'Olá, eu me chamo ' . $nome . ' e tenho ' . $idade . ' anos';
+    ?>
+</body>
+</html>
---
+++ variaveis.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Studies</title>
+</head>
+<body>
+    <?php
+        $nome = 'Caroline Trombeta';
+        $idade = 26;
+    ?>
+
+    <h3>
+        Meu nome é <?= $nome ?> e eu tenho <?= $idade ?> anos.
+    </h3>
+</body>
+</html>
