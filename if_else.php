3e5dcf4917a264d5545e35554cd02b82aebb6ab2
Caroline Trombeta <carolinecampost@gmail.com>
2022-12-07 11:07:03 +0000

    07/12 - if/else

---
+++ if_else.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>If/else</title>
+</head>
+<body>
+    <?php
+        /*
+            Igual ==
+            Identico ===
+            Diferente =! OU <>
+            Não identico !==
+            Menor <
+            Maior >
+            Menor ou igual <=
+            Maior ou igual >=
+        */
+
+        if(2 === 2) {
+            echo 'Verdadeiro';
+        } else {
+            echo 'Falso';
+        }
+    ?>
+</body>
+</html>
